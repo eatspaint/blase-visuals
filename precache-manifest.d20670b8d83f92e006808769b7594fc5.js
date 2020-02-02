7edd971f9052d84ddb46c349c7456689c5828fd6
eatspaint <cef2010@gmail.com>
2020-02-01 20:15:31 -0800

    Updates

---
+++ precache-manifest.d20670b8d83f92e006808769b7594fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770b2e5d080208600fb33021da731860",
+    "url": "/blase-visuals/index.html"
+  },
+  {
+    "revision": "312d3290f0be12ddb673",
+    "url": "/blase-visuals/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "312d3290f0be12ddb673",
+    "url": "/blase-visuals/static/js/2.c033a896.chunk.js"
+  },
+  {
+    "revision": "3811c12d7931687dc91769ed6aad93b2",
+    "url": "/blase-visuals/static/js/2.c033a896.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3699b46a0efb7fb54bd",
+    "url": "/blase-visuals/static/js/main.009c4a9f.chunk.js"
+  },
+  {
+    "revision": "65c6c7a173adbd1518cb",
+    "url": "/blase-visuals/static/js/runtime-main.ad417597.js"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/blase-visuals/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  }
+]);
